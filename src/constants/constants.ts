d73849e0c6f1dead6332575ec49f7d71f9f8a359
Mert Gürer <mert.gurer@hotmail.com.tr>
2024-12-09 01:10:53 +0300

    update

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -3,7 +3,7 @@ export const REGEX = {
 };
 
 export const EMAIL_JS = {
-    serviceId: "service_9m2qyly",
+    serviceId: "service_xou32mv",
     templateId: "template_xke7y6o",
     publicKey: "PVH-QMvbDLruPiE7f",
 };
